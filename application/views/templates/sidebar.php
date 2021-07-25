5ab249d1469775dcebaf583bb94e017c44562781
insanz01 <insankamil002@gmail.com>
2021-07-25 13:57:36 +0700

    change template

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,32 +1,40 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background: #87cefa !important;">
   <!-- Brand Logo -->
   <a href="<?= base_url('app/index') ?>" class="brand-link">
     <img src="<?= base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
          style="opacity: .8">
-    <span class="brand-text font-weight-light">Diagnosis SP</span>
+    <span class="brand-text font-weight-light" style=" color: #252525; !important">Diagnosis SP</span>
   </a>
 
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="<?= base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+    <div class="user-panel mt-3 pb-3 mb-3">
+      <div class="d-flex mt-2">
+        <div class="info">
+          <p class="text-center font-weight-bold">Sistem Pakar Diagnosis <br> Awal Gangguan Kecemasan</p>
+        </div>
       </div>
-      <div class="info">
-        <?php if($this->session->userdata('sess_user_id')): ?>
-          <?php if($this->session->userdata('sess_role_id') == 1): ?>
-            <a href="#" class="d-block">Admin Pusat</a>
-          <?php elseif($this->session->userdata('sess_role_id') == 3): ?>
-            <a href="#" class="d-block">Pakar Pusat</a>
+      <hr style="color: black !important;">
+      <div class="d-flex">
+        <div class="image">
+          <img src="<?= base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info" style="color: #252525; !important">
+          <?php if($this->session->userdata('sess_user_id')): ?>
+            <?php if($this->session->userdata('sess_role_id') == 1): ?>
+              <a href="#" class="d-block"style=" color: #252525; !important">Admin Pusat</a>
+            <?php elseif($this->session->userdata('sess_role_id') == 3): ?>
+              <a href="#" class="d-block"style=" color: #252525; !important">Pakar Pusat</a>
+            <?php else: ?>
+              <a href="#" class="d-block"style=" color: #252525; !important">Member Pusat</a>
+            <?php endif; ?>
           <?php else: ?>
-            <a href="#" class="d-block">Member Pusat</a>
+            <a href="#" class="d-block"style=" color: #252525; !important">Belum Login</a>
           <?php endif; ?>
-        <?php else: ?>
-          <a href="#" class="d-block">Belum Login</a>
-        <?php endif; ?>
-      </div>
+        </div>
+      </div>    
     </div>
 
     <!-- Sidebar Menu -->
@@ -36,7 +44,7 @@
              with font-awesome or any other icon font library -->
              <!-- tambah class menu-open untuk secara otomatis membuka -->
         <li class="nav-item">
-          <a href="<?= base_url('app/index') ?>" class="nav-link">
+          <a href="<?= base_url('app/index') ?>" class="nav-link" style=" color: #252525; !important">
             <i class="nav-icon fas fa-home"></i>
             <p>
               Dashboard
@@ -46,7 +54,7 @@
         </li>
         <?php if($this->session->userdata('sess_role_id') != 1): ?>
           <li class="nav-item">
-            <a href="<?= base_url('app/konsultasi') ?>" class="nav-link">
+            <a href="<?= base_url('app/konsultasi') ?>" class="nav-link" style=" color: #252525; !important">
               <i class="nav-icon fas fa-person-booth"></i>
               <p>
                 Konsultasi
@@ -55,7 +63,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= base_url('app/edukasi') ?>" class="nav-link">
+            <a href="<?= base_url('app/edukasi') ?>" class="nav-link" style=" color: #252525; !important">
               <i class="nav-icon fas fa-book"></i>
               <p>
                 Edukasi
@@ -72,7 +80,7 @@
             </a>
           </li> -->
           <li class="nav-item">
-            <a href="<?= base_url('app/bantuan') ?>" class="nav-link">
+            <a href="<?= base_url('app/bantuan') ?>" class="nav-link" style=" color: #252525; !important">
               <i class="nav-icon fas fa-hands-helping"></i>
               <p>
                 Bantuan
@@ -82,7 +90,7 @@
         <?php endif; ?>
         <?php if(!$this->session->userdata('sess_user_id')): ?>
           <li class="nav-item">
-            <a href="<?= base_url('app/login') ?>" class="nav-link">
+            <a href="<?= base_url('app/login') ?>" class="nav-link" style=" color: #252525; !important">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Akun
@@ -92,9 +100,9 @@
           </li>
         <?php else: ?>
           <?php if($this->session->userdata('sess_role_id') == 3): ?>
-            <li class="nav-header">PAKAR AREA</li>
+            <li class="nav-header" style=" color: #252525; !important">PAKAR AREA</li>
             <li class="nav-item has-treeview">
-              <a href="#" class="nav-link">
+              <a href="#" class="nav-link" style=" color: #252525; !important">
                 <i class="nav-icon fas fa-users"></i>
                 <p>
                   Master Data
@@ -103,19 +111,19 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="<?= base_url('admin/gejala') ?>" class="nav-link">
+                  <a href="<?= base_url('admin/gejala') ?>" class="nav-link" style=" color: #252525; !important">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Gejala</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="<?= base_url('admin/penyakit') ?>" class="nav-link">
+                  <a href="<?= base_url('admin/penyakit') ?>" class="nav-link" style=" color: #252525; !important">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Penyakit</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="<?= base_url('admin/certainty') ?>" class="nav-link">
+                  <a href="<?= base_url('admin/certainty') ?>" class="nav-link" style=" color: #252525; !important">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Certainty Factor</p>
                   </a>
@@ -129,9 +137,9 @@
               </ul>
             </li>
           <?php elseif($this->session->userdata('sess_role_id') == 1): ?>
-            <li class="nav-header">ADMIN AREA</li>
+            <li class="nav-header" style=" color: #252525; !important">ADMIN AREA</li>
             <li class="nav-item has-treeview">
-              <a href="#" class="nav-link">
+              <a href="#" class="nav-link" style=" color: #252525; !important">
                 <i class="nav-icon fas fa-users"></i>
                 <p>
                   Master Data
@@ -140,13 +148,13 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="<?= base_url('admin/akun/pakar') ?>" class="nav-link">
+                  <a href="<?= base_url('admin/akun/pakar') ?>" class="nav-link" style=" color: #252525; !important">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Akun Pakar</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="<?= base_url('admin/akun/member') ?>" class="nav-link">
+                  <a href="<?= base_url('admin/akun/member') ?>" class="nav-link" style=" color: #252525; !important">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Akun Member</p>
                   </a>
@@ -155,7 +163,7 @@
             </li>
           <?php endif; ?>
           <li class="nav-item">
-            <a href="<?= base_url('app/logout') ?>" class="nav-link">
+            <a href="<?= base_url('app/logout') ?>" class="nav-link" style=" color: #252525; !important">
               <i class="nav-icon fas fa-sign-out-alt"></i>
               <p>
                 Keluar
